6ce5353f6ca8fa025c0996893b4a66bf2e8e65d2
meerim05 <meerimt005@gmail.com>
2021-08-15 01:17:42 -0400

    My second commit

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -2,8 +2,9 @@ package com.collection;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i <5; i++) {
+        for (int i = 0; i <4; i++) {
             System.out.println("Hello Batch 9");
+            System.out.println("LAdwjff");
             Thread.sleep(2000);
         }
     }
